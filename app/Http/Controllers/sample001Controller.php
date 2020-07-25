33c44eb936c5146c81cf74793dcf7de628183039
moto1408 <m.hayashi1408@gmai.com>
2020-07-26 04:13:29 +0900

    insert処理追加

--- app/Http/Controllers/sample001Controller.php
+++ app/Http/Controllers/sample001Controller.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\DB;
 // 入力チェッククラス
 use App\Http\Requests\sample001Request;
 
+use App\User;
+
 class sample001Controller extends Controller
 {
 	public function __construct()
@@ -33,6 +35,9 @@ class sample001Controller extends Controller
 		
 		
 		$recodes = DB::table('users')->where($where)->get();
+		
+		$recodes = User::all();
+		// var_dump($neko);
 		return view('sample001.index',compact('recodes'));
 	}
 
@@ -49,7 +54,7 @@ class sample001Controller extends Controller
 		// 名前｜必須
 		$validate_rule['name'] = 'required';
 		// メール｜フォーマット
-		$validate_rule['email'] = 'required|email';
+		$validate_rule['email'] = 'required|email|unique:users,email';
 		//　年齢｜数値、0～150
 		$validate_rule['age'] = 'numeric|between:0,150';
 
@@ -64,16 +69,17 @@ class sample001Controller extends Controller
                         ->withInput();
 		}
 		
-		$param = array();
+		// $param = array();
 		// $param['name'] = $request->input('name');
 		// $param['email'] = $request->input('email');
 		// $param['age'] = $request->input('age');
-		$param = [
-			'name' => $request->input('name'),
-			'email' => $request->input('email'),
-			'age' => $request->input('age'),
-			'password' => ''
-		];
+		// $param['password'] = '';
+		// $param = [
+		// 	'name' => $request->input('name'),
+		// 	'email' => $request->input('email'),
+		// 	'age' => $request->input('age'),
+		// 	'password' => ''
+		// ];
 
 		// $insert_sql = 
 		// 	"INSERT INTO users"
@@ -81,8 +87,38 @@ class sample001Controller extends Controller
 		// 	. "VALUES "
 		// 		. "( :name, :email, :age, :password);";
 		// DB::insert($insert_sql,$param);
-		DB::table('users')->insert($param);
+		// DB::table('users')->insert($param);
 
+		// モデル呼び出し
+		$ModelUser = new User;
+		// トランザクション用意
+		DB::beginTransaction();
+		try{
+			// 各値をセットする
+			// 名前
+			$ModelUser->name = $request->name;
+			// メール
+			$ModelUser->email = $request->email;
+			// 年齢
+			$ModelUser->age = $request->age;
+			// パスワード
+			$ModelUser->password = '';
+			// 登録日時
+			$ModelUser->created_at = time();
+			// 更新日時
+			$ModelUser->updated_at = time();
+
+			// 登録実行
+			$ModelUser->save();
+			// コミット
+			DB::commit();
+		}catch(Exception $e){
+			// ロールバック
+			DB::rollback();
+		}
+		
+
+		
 		
 		$method_name = __METHOD__;
 		// $temp = $this->validate($request,$validate_rule);
